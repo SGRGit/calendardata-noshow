0d9357a7bddc25875055318d9683ca6d89070402
SGRGit <50324151+SGRGit@users.noreply.github.com>
2020-01-09 14:38:16 +0530

    Add files via upload

---
+++ calendardata.py
@@ -0,0 +1,40 @@
+import json
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/event_calender')
+def event_calender():
+	finalList = []
+	finalDict = {'date': '', 'event': []}
+
+	eventList = []
+	nested_dict = {'name': '', 'value':'', 'color':''}
+	
+	nested_dict['name'] = 'appointment'
+	nested_dict['value'] = '132'
+	nested_dict['color'] = 'green'
+	
+	eventList.append(dict(nested_dict))
+
+	nested_dict['name'] = 'confirmed'
+	nested_dict['value'] = '13'
+	nested_dict['color'] = 'brown'
+	
+	eventList.append(dict(nested_dict))
+
+	finalDict['date'] = '2020-01-01'
+	finalDict['event'] = eventList
+
+	finalList.append(dict(finalDict))
+
+	finalDict['date'] = '2020-01-02'
+	finalDict['event'] = eventList
+
+	finalList.append(dict(finalDict))
+
+	return jsonify(finalList)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='7000', threaded=True, debug=True)
