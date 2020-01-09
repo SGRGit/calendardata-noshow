73d525a2cf7c19e6b836bf225562d1cbbf5ea8a1
SGRGit <50324151+SGRGit@users.noreply.github.com>
2020-01-09 19:48:08 +0530

    Update calendardata.py

--- calendardata.py
+++ calendardata.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}))
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/', methods=['GET'])
 def event_calender():
